2cd512a8ba2ce4d48bef1465257643611863e15d
MAGA <magalivanesapommiersc@gmail.com>
2025-06-26 16:00:32 -0300

    TP17

---
+++ TP17/script/script1.js
@@ -0,0 +1,11 @@
+console.log("soy el archivo externo, hola");
+console.info("soy una info");
+console.warn("soy un warn");
+console.error("soy un error");
+
+console.table({
+    Nombre: "Pablo" ,
+    Apellido: "Deganis" ,
+    Curso: "Programacion" ,
+})
+window.prompt("Ingrese un dato");
---
+++ TP17/script/script2.js
@@ -0,0 +1,36 @@
+let miVariable= "info";
+console.log(miVariable);
+miVariable= 558;
+console.log(miVariable);
+const apellido = "Deganis";
+/*console.log(apellido)
+*/
+const nombre = "Pablo";
+console.log(nombre , apellido);
+const nombreCompleto = nombre + apellido;
+console.log(nombreCompleto);
+/*const dni= window.prompt("Ingrese su DNI");
+console.log(dni);*/
+let numeroA = 6;
+let numeroB = 4;
+let suma = numeroA + numeroB;
+console.log(suma)
+/*let numeroC = window.prompt("Primer numero");
+numeroC = Number(numeroC);
+let numeroD = window.prompt("Segundo numero");
+numeroD = Number(numeroD);
+let sumaCD = numeroC + numeroD;
+console.log(sumaCD);*/
+
+//Scoope
+
+let numeroX = 17;
+console.log(numeroX)
+{
+    let numeroX = 18;
+console.log(numeroX)
+let numeroy = 67;
+
+}
+console.log(numeroX)
+console.log(numeroy)
