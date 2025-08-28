b2d771f3a1960f5fc439b9bc1ecff7e59e57793a
MAGA <magalivanesapommiersc@gmail.com>
2025-08-28 15:52:32 -0300

    TP24

--- TP24/script/script2.js
+++ TP24/script/script2.js
@@ -26,6 +26,48 @@ document.querySelector("#calcular").addEventListener("click", () => {
         mensaje.textContent = "Error! escriba su nombre en el input"
     } else {
         //console.log("Input con info: " * nombre.value);
-        mensaje.textContent = "ingredientes " + papas * cantidad.value
+        mensaje.innerHTML = "ingredientes " + papas * cantidad.value
+    }
+
+
+
+})
+document.querySelector("#listaAsistencias").addEventListener("click", () => {
+    const nombre = document.querySelector("#agregar");
+    const mensaje = document.querySelector("#ver");
+})
+
+let asistentes = [];
+document.querySelector("#agregar").addEventListener("click", () => {
+    //console.log("boton agregar clickeado");
+    const asistente = document.querySelector("#asistente");
+    const mensaje = document.querySelector("#listaAsistencias #mensaje");
+    if (asistente.value === "") {//mensaje de error
+
+        mensaje.textContent = "Error! Ingrese un nombre en el input";
+    } else {//grabo string en array
+        asistentes.push(asistente.value);
+        mensaje.textContent = "...resouesta"
+    }
+    asistentes.push(asistente.value);
+    asistente.value = "";
+})
+document.querySelector("#ver").addEventListener("click", () => {
+    //console.log("boton ver clickeado");
+    const mensaje = document.querySelector("#listaAsistencias #mensaje");
+    //mensaje.textContent = asistentes;
+    mensaje.textContent = "";
+    if (asistentes.lenght === 0) {
+        //indicar que no hay datos
+        mensaje.textContent = "Error! No hay nombres guardados";
+    } else {
+        //mpostrar los datos del array
+        asistentes.forEach((nombre, i) => {
+            //console.log(nombre);
+            i++;
+            mensaje.innerHTML += `<div>${i} - ${nombre}</div>`;
+        });
     }
 })
+
+
